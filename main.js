5975d79e3d176f4e26173368e3ebc47c080446aa
masudurrahman <masudurrtr01@gmail.com>
2022-02-12 13:00:54 +0600

    updated...

--- main.js
+++ main.js
@@ -46,37 +46,37 @@ tab_menu_el.forEach(items => {
 
     })   
 })
+//  Elements for tabs end here
 
 
 
-// Get elements for students result sheet start here
-const student_form = document.getElementById('student_form')
-const data_list = document.getElementById('data_list')
+// get element for student data sheet start here
+const student_form = document.getElementById('student_form');
+const data_list = document.getElementById('data_list');
 
 student_form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    let name = student_form.querySelector("input[placeholder='Student Name']")
-    let roll = student_form.querySelector("input[placeholder='Roll Number']") 
-    let student_class = student_form.querySelector("input[placeholder='Class']")
-    let photo = student_form.querySelector("input[placeholder='Photo']")
-    let gender = student_form.querySelector("input[type='radio']:checked")
-    let ban = student_form.querySelector("input[placeholder='Bangla']")
-    let eng = student_form.querySelector("input[placeholder='English']")  
-    let math = student_form.querySelector("input[placeholder='Math']")
-    let sci = student_form.querySelector("input[placeholder='Science']") 
-    let ss = student_form.querySelector("input[placeholder='Social Science']")
-    let reli = student_form.querySelector("input[placeholder='Religion']") 
+    let name = student_form.querySelector("input[placeholder='Student Name']"); 
+    let roll = student_form.querySelector("input[placeholder='Roll Number']"); 
+    let student_class = student_form.querySelector("input[placeholder='Class']"); 
+    let photo = student_form.querySelector("input[placeholder='Photo']"); 
+    let gender = student_form.querySelector("input[type='radio']:checked");  
+    let ban = student_form.querySelector("input[placeholder='Bangla']");  
+    let eng = student_form.querySelector("input[placeholder='English']");  
+    let math = student_form.querySelector("input[placeholder='Math']");  
+    let sci = student_form.querySelector("input[placeholder='Science']");  
+    let ss = student_form.querySelector("input[placeholder='Social Science']");  
+    let reli = student_form.querySelector("input[placeholder='Religion']"); 
+    
     
     if(name.value == '' || roll.value == '' || student_class.value == '' ){
-        alert('All fields are required ')
-         
+        alert('All fields are required ');
     }else {
        
-        let storate_data = []
-
+        let storate_data = [];
         if( dataGet('result_apps') ){
-            storate_data = dataGet('result_apps')
+            storate_data = dataGet('result_apps');
         }
 
         storate_data.push({
@@ -91,89 +91,95 @@ student_form.addEventListener('submit', function(e){
             sci : sci.value, 
             social : ss.value, 
             reli : reli.value 
-        })
+        });
 
-        dataSend('result_apps', storate_data)
+        dataSend('result_apps', storate_data);
 
-        student_form.querySelector("input[placeholder='Student Name']").value = ''
-        student_form.querySelector("input[placeholder='Roll Number']").value = ''
-        student_form.querySelector("input[placeholder='Class']").value = ''
-        student_form.querySelector("input[placeholder='Photo']").value = ''     
-        student_form.querySelector("input[placeholder='Bangla']").value = '' 
-        student_form.querySelector("input[placeholder='English']").value = ''  
-        student_form.querySelector("input[placeholder='Math']").value = '' 
-        student_form.querySelector("input[placeholder='Science']").value = '' 
-        student_form.querySelector("input[placeholder='Social Science']").value = ''  
-        student_form.querySelector("input[placeholder='Religion']").value = ''
-        
-       allStudentData()
+        student_form.querySelector("input[placeholder='Student Name']").value = ''; 
+        student_form.querySelector("input[placeholder='Roll Number']").value = ''; 
+        student_form.querySelector("input[placeholder='Class']").value = ''; 
+        student_form.querySelector("input[placeholder='Photo']").value = '';     
+        student_form.querySelector("input[placeholder='Bangla']").value = '';  
+        student_form.querySelector("input[placeholder='English']").value = '';  
+        student_form.querySelector("input[placeholder='Math']").value = '';  
+        student_form.querySelector("input[placeholder='Science']").value = '';  
+        student_form.querySelector("input[placeholder='Social Science']").value = '';  
+        student_form.querySelector("input[placeholder='Religion']").value = ''; 
+                    
+        allStudentData();
     }
     
-})
+});
 
-allStudentData()
+allStudentData();
 
 function allStudentData(){
 
-    let all_data = dataGet('result_apps')
+    let all_data = dataGet('result_apps');
 
-    let data = ''
-
+    let data = '';
+    let result = new Result;
+    
     all_data.map((student, index) => {
         data += `
-            <tr class="text-center">
-                <td>${ index + 1 }</td>
-                <td>${ student.name }</td>
-                <td>${ student.roll }</td>
-                <td>${ student.className }</td>
-                <td>${ student.gender }</td>
-                <td> A </td>
-                <td> 4 </td>
+            <tr>
+                <td class="pt-4">${ index + 1 }</td>
+                <td class="pt-4">${ student.name }</td>
+                <td class="pt-4">${ student.roll }</td>
+                <td class="pt-4">${ student.className }</td>
+                <td class="pt-4">${ student.gender }</td>
+                
+                <td class="pt-4"> ${ result.finalCgpa(all_data[index].ban, all_data[index].eng, all_data[index].math, all_data[index].sci, all_data[index].social, all_data[index].reli).resgread } </td>
+                
+                <td class="pt-4"> ${ result.finalCgpa(  all_data[index].ban, all_data[index].eng, all_data[index].math, all_data[index].sci, all_data[index].social, all_data[index].reli  ).rescgpa }</td>
+                
                 <td><img style="width:50px; height:50px;object-fit:cover;" src="${ student.photo }"></td>
-                <td class="d-flex">
-                    <button class="btn btn-info btn-sm mr-1" data-toggle="modal" onclick="getSingleResult(${index})" data-target="#student_single_modal" >View</button>
+                <td class="pt-3" >
+                    <button class="btn btn-info btn-sm me-1" data-bs-toggle="modal" onclick="getSingleResult(${index})" data-bs-target="#student_single_modal" >View</button>
                     <button onclick="deleteStudent(${ index })" class="btn btn-danger btn-sm">Delete</button>
                 </td>
             </tr>
-        `
-    })
+        `;
+    });
 
-    data_list.innerHTML = data
+    data_list.innerHTML = data;
 
 }
+// 
 
 
-// Delete Student Data 
+
+/**
+ * Delete Student Data 
+ */
 
 function deleteStudent(id){
 
-    let confirmation = confirm('Are you sure ?')
+    let conf = confirm('Are you sure ?');
 
-    if(confirmation){
-        let storage_data = dataGet('result_apps')
+    if(conf){
+        let storage_data = dataGet('result_apps');
         storage_data.splice(id, 1);
-        dataSend('result_apps',storage_data)
+        dataSend('result_apps',storage_data);
         allStudentData();
     }else{
-        return false
+        return false;
     }
 
 }
-// For result sheet
-const student_result_data = document.querySelector('.student-result-data')
+
+const student_result_data = document.querySelector('.student-result-data');
 
 function getSingleResult(index){
+    let result = new Result;
+    let storage_data = dataGet('result_apps');
     
-    let result = new Result
-
-    let storage_data = dataGet('result_apps')
-
     student_result_data.innerHTML = `
-            <img <img style="width:250px; height:250px;object-fit:cover;" class="img-thumbnail shadow rounded" src="${ storage_data[index].photo }" alt="">
-            <h2 class="mt-1">${ storage_data[index].name }</h2>
+            <img style="width:250px; height:250px;object-fit:cover;" class="shadow img-thumbnail" src="${ storage_data[index].photo }" alt="">
+            <h2>${ storage_data[index].name }</h2>
             <hr>
             <table class="table table-striped table-bordered">
-                <thead class= "bg-dark text-white">
+                <thead class = "bg-dark text-white text-center">
                     <tr>
                         <th>Subject</th>
                         <th>Marks</th>
@@ -189,11 +195,12 @@ function getSingleResult(index){
                         <td>${ storage_data[index].ban }</td>
                         <td>${ result.result(storage_data[index].ban).gpacal }</td>
                         <td>${ result.result(storage_data[index].ban).greadcal }</td>
-                        <td rowspan="6">                       
-                        ${ result.finalCgpa(storage_data[index].ban, storage_data[index].eng, storage_data[index].math, storage_data[index].sci, storage_data[index].social, storage_data[index].reli).rescgpa }
-                                              
+                        <td rowspan="6">  
+
+                        ${ result.finalCgpa(  storage_data[index].ban, storage_data[index].eng, storage_data[index].math, storage_data[index].sci, storage_data[index].social, storage_data[index].reli  ).rescgpa }
+                        
                         </td> 
-                        <td rowspan="6" >${ result.finalCgpa(  storage_data[index].ban, storage_data[index].eng, storage_data[index].math, storage_data[index].sci, storage_data[index].social, storage_data[index].reli  ).resgread }</td>
+                        <td rowspan="6">${ result.finalCgpa(  storage_data[index].ban, storage_data[index].eng, storage_data[index].math, storage_data[index].sci, storage_data[index].social, storage_data[index].reli  ).resgread }</td>
                     </tr>
                     <tr>
                         <td>English</td>
@@ -228,10 +235,10 @@ function getSingleResult(index){
                         <td>${ storage_data[index].reli }</td>
                         <td>${ result.result(storage_data[index].reli).gpacal }</td>
                         <td>${ result.result(storage_data[index].reli).greadcal }</td>
-
                     </tr>
                 </tbody>
-            </table>        
+            </table>
+            
     `;
 }
 
